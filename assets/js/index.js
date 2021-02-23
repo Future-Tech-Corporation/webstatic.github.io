fc0f0fbdc317d899f44417e726c786a1682802a6
Future-Tech Corporation <79355659+Future-Tech-Corporation@users.noreply.github.com>
2021-02-23 16:28:49 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,5 @@
 'use strict';
 
-var str = "";
-
-  var input = document.getElementsByTagName("input")[0];
-
-  var span = document.getElementsByTagName("span")[0];
-  input.oninput = function(evt) {
-    var val = this.value;
-    if (val.length > str.length) {
-    
-      str += val.charAt(val.length - 1);
-    } else {
-    
-      str = str.substr(0, val.length);
-    }
-    
-    this.value =
-      val.substr(0, val.length - 1).replace(/./g, "*") +
-      val.charAt(val.length - 1);
-	  
-    var len = this.value.length;
-    setTimeout(() => {
-
-      if (this.value.length == len) {
-
-        this.value = this.value.replace(/./g, "*");
-      }
-    }, 1000);
-    span.innerHTML = str;
-  };
-
 var placeholders = document.querySelectorAll('.styled-input__placeholder-text'),
     inputs = document.querySelectorAll('.styled-input__input');
 
